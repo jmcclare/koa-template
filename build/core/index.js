c08f804999cac6881641c3109d408db56c545591
Jonathan McClare <git.jmcclare@recursor.net>
2018-04-10 17:19:12 -0400

    add generated fake product list

--- build/core/index.js
+++ build/core/index.js
@@ -130,8 +130,27 @@ app.use((0, _koaStatic2.default)(_path2.default.join(__dirname, '../public')));
 //app.use (ctx, next) =>
 //throw new Error 'Wolf!'
 productsRouter.get('products', '/', function (ctx, next) {
+  var buildProd, i, len, name, productNames, products;
+  productNames = ['ASUS X542BA-DH99', 'iSmart SMTB4002 N14H Pro-R', 'ASUS VivoBook E403NA-US21', 'ASUS Vivobook S S510UA-DS71', 'ASUS R541NA-RS01 Notebook', 'ASUS ZenPad 8', 'ASUS Transformer Book T101HA-C4-GR', 'ASUS Zenpad Z500M-C1-SL', 'Lenovo ThinkPad E570', 'Acer (Refurbished) Aspire A315-21-99E5', 'ASUS F542UA-DH71', 'Acer (Refurbished) Swift SF314-52-53AX', 'ASUS Transformer Mini T102HA-D4-GR', 'Lenovo Thinkpad T440', 'ASUS VivoBook X540UA-DH31', 'HP(Refurbished) 210 G1', 'iSmart SMTB4100', 'ASUS ZenBook UX430UQ-Q72SP-CB', 'Acer Swift 3 SF314-52-55HL', 'Acer (Refurbished) Spin 3 SP315-51-598W'];
+  products = [];
+  buildProd = function buildProd(name) {
+    var newProd, price;
+    price = Math.round((Math.random() * 2300 + 300) * 100) / 100;
+    newProd = {
+      name: name,
+      price: price
+    };
+    return products.push(newProd);
+  };
+  for (i = 0, len = productNames.length; i < len; i++) {
+    name = productNames[i];
+    buildProd(name);
+  }
+  console.log('in products route');
+  console.log(products);
   return ctx.render('products', {
-    title: 'Products'
+    title: 'Products',
+    products: products
   }, true);
 });
 
--- core/index.coffee
+++ core/index.coffee
@@ -93,7 +93,39 @@ app.use serve path.join __dirname, '../public'
 
 
 productsRouter.get 'products', '/', (ctx, next) =>
-  ctx.render 'products', { title: 'Products' }, true
+  productNames = [
+    'ASUS X542BA-DH99'
+    'iSmart SMTB4002 N14H Pro-R'
+    'ASUS VivoBook E403NA-US21'
+    'ASUS Vivobook S S510UA-DS71'
+    'ASUS R541NA-RS01 Notebook'
+    'ASUS ZenPad 8'
+    'ASUS Transformer Book T101HA-C4-GR'
+    'ASUS Zenpad Z500M-C1-SL'
+    'Lenovo ThinkPad E570'
+    'Acer (Refurbished) Aspire A315-21-99E5'
+    'ASUS F542UA-DH71'
+    'Acer (Refurbished) Swift SF314-52-53AX'
+    'ASUS Transformer Mini T102HA-D4-GR'
+    'Lenovo Thinkpad T440'
+    'ASUS VivoBook X540UA-DH31'
+    'HP(Refurbished) 210 G1'
+    'iSmart SMTB4100'
+    'ASUS ZenBook UX430UQ-Q72SP-CB'
+    'Acer Swift 3 SF314-52-55HL'
+    'Acer (Refurbished) Spin 3 SP315-51-598W'
+  ]
+  products = []
+  buildProd = (name) ->
+    price = Math.round(((Math.random() * 2300) + 300) * 100) / 100
+    newProd =
+      name: name
+      price: price
+    products.push newProd
+  buildProd name for name in productNames
+  console.log 'in products route'
+  console.log products
+  ctx.render 'products', { title: 'Products', products: products }, true
 
 productsRouter.get 'react-sample', '/react-sample', (ctx, next) =>
   ctx.render 'react-sample', { title: 'React Sample' }, true
