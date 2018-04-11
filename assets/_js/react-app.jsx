19222cadb2d6e4c07b9998094ba86f6fa63b8936
Jonathan McClare <git.jmcclare@recursor.net>
2018-04-11 14:59:39 -0400

    add sample Tic Tac Toe react app
    I also added some documentation to the sample React app page to explain
    where its files are in the project.

--- assets/_js/react-app.jsx
+++ assets/_js/react-app.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Swiggity from './swiggity'
 import Toggle from './toggle'
+import TicTacToe from './tictactoe'
 
 console.log('Console message from react-app.jsx')
 
@@ -11,7 +12,12 @@ console.log('Console message from react-app.jsx')
   //document.getElementById('react-test')
 //)
 
+//ReactDOM.render(
+  //<Toggle />,
+  //document.getElementById('react-test')
+//)
+
 ReactDOM.render(
-  <Toggle />,
-  document.getElementById('react-test')
+  <TicTacToe />,
+  document.getElementById('tic-tac-toe')
 )
